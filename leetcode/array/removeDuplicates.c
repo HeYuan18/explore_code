f3391938dbb63cb41305ba3a003822b5c3fb8119
HeYuan <1191927665@qq.com>
2018-04-21 15:42:03 +0800

    ~.~编程练习

---
+++ leetcode/array/removeDuplicates.c
@@ -0,0 +1,117 @@
+/*************************************************************************
+	> File Name: removeDuplicates.c
+	> Author: HeYuan
+	> Mail: HeYuan951@Gmail.com
+	> Created Time: 2018年04月15日 星期日 20时29分05秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef int* Array;
+
+Array init_array(Array arr, int len)
+{
+    arr = (Array)malloc(sizeof(int) * len);
+    if(NULL == arr)
+    {
+        return NULL;
+    }
+
+    memset(arr, 0, len);
+
+    return arr;
+}
+
+Array input_array(Array arr, int len)
+{
+    printf("input: ");
+    int i = 0;
+    for(; i < len; ++i)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    return arr;
+}
+
+int removeDuplicates(Array arr, int len)
+{
+    if(NULL == arr)
+    {
+        return 0;
+    }
+    if(1 == len)
+    {
+        return 1;
+    }
+    
+    int i = 0, j = 0;
+    int c = 0;
+    
+    for(; i < len; ++i)
+    {
+        for(j = i + 1; j < len; ++j)
+        {
+            if(arr[i] != arr[j])
+            {
+                continue;
+            }
+            else
+            {
+                if(j <)
+                arr[i] = arr[j + 1];
+                ++c;
+            }
+        }
+    }
+    
+    return c;
+}
+
+void print_array(Array arr, int len)
+{
+    printf("print: ");
+    int i = 0;
+    for(; i < len; ++i)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+Array destory_array(Array arr)
+{
+    if(NULL != arr)
+    {
+        free(arr);
+        arr = NULL;
+    }
+
+    return arr;
+}
+
+int main()
+{
+    int len = 0;
+    printf("input len: ");
+    scanf("%d", &len);
+    if(0 == len)
+    {
+        printf("InvaildInput\n");
+
+        return 0;
+    }
+
+    Array Arr = NULL;
+    Arr = init_array(Arr, len);
+    Arr = input_array(Arr, len);
+    
+    int num = removeDuplicates(Arr, len);
+    print_array(Arr, num);
+
+    Arr = destory_array(Arr);
+
+    return 0;
+}
